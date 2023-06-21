2f98ec947cb92e2e1a45a5f4b0363bedc3c47112
HFarahat <86498731+FarahatDeploys@users.noreply.github.com>
2023-06-21 10:17:33 +0300

    Create CountPunct.c

---
+++ CountPunct.c
@@ -0,0 +1,27 @@
+// Author Farahat
+// Data 20-6-23
+
+// compute the standarad deviation !
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int count_punct(char *s);
+int main()
+{
+    char s[] = "apple, pear, orange; something...";
+    int total = count_punct(s);
+    printf("total: %d\n",total);
+    return 0;
+}
+int count_punct(char *s)
+{
+    int len = strlen(s);
+    int count = 0;
+    for(int i =0;i<len;i++){
+        if(ispunct(s[i])) count++;
+    }
+    return count;
+}
+
+
